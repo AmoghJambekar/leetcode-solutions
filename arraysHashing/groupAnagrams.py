dc8489445938db2cda4cd9e2a238a55cc868f36f
Amogh Jambekar <amoghjambekar45@gmail.com>
2026-06-21 22:17:13 -0400

    feat: updates group anagrams to be 7 ms faster

--- arraysHashing/groupAnagrams.py
+++ arraysHashing/groupAnagrams.py
@@ -1,10 +1,18 @@
 class Solution(object):
     def groupAnagrams(self, strs):
+
         groups = {}
+        
+        if strs < 0:
+            return [""]
+
         for word in strs:
             key = ''.join(sorted(word))
-            if not(key in groups):
-                groups[key] = [word]
-            else:
+            if key in groups:
                 groups[key].append(word)
+            else:
+                groups[key] = [word]
         return groups.values()
+
+    
+        
